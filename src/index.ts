c6718d907e26dd0a29d9f46faabe21d5f31d7bc3
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2025-12-05 18:05:19 +0200

    feat: use bun sql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { neon } from '@neondatabase/serverless'
+import { sql } from 'bun'
 import type { PersonalInfo } from '@/types'
 
 const PAT = process.env.OURA_PAT
-const DB_URL = process.env.DATABASE_URL
 
 const getPersonalInfo = async (): Promise<PersonalInfo> => {
   const response = await fetch('https://api.ouraring.com/v2/usercollection/personal_info', {
@@ -14,10 +13,8 @@ const getPersonalInfo = async (): Promise<PersonalInfo> => {
   return (await response.json()) as PersonalInfo
 }
 
-const sql = neon(DB_URL)
-
 const rows = await sql`SELECT version()`
-console.log(rows[0]?.['version'])
+console.log(rows)
 
 const personalInfo = await getPersonalInfo()
 console.log(personalInfo)
